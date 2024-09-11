02fc113506f6d465a7eeb9dc13019e4571aaa2a7
wenyan <3247650181@qq.com>
2024-09-11 19:16:19 +0800

    feat: eslint

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
- 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [vue()],
+    build: {
+        minify: 'true',
+        terserOptions: {
+            compress: {
+                //生产环境时移除console
+                drop_console: true,
+                drop_debugger: true,
+            },
+        },
+    },
 })
